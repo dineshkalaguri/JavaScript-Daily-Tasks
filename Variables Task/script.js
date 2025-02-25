23974d65f05773b312ad7a99b33bcef70560b77b
dineshkalaguri <dineshkalaguri@gmail.com>
2025-02-25 21:35:55 +0530

    Add files via upload

---
+++ Variables Task/script.js
@@ -0,0 +1,100 @@
+
+// 1.Variables //
+
+var favoritecolor = "Red"; 
+let favoritenumber = 1847; 
+const favoritename = "Dinesh"; 
+
+console.log(favoritecolor);// Output: Red//
+console.log(favoritenumber);// Output:1847 //
+console.log(favoritename);// Output:Dinesh //
+
+// 2.Data Types //
+
+console.log(typeof "42"); // Output:string //
+console.log(typeof 42); // Output: number//
+console.log(typeof true); // Output:boolean //
+console.log(typeof undefined); // Output:undefined //
+console.log(typeof ["apple","banana"]); // Output:object //
+console.log(typeof null); // Output:object //
+
+// 3.Numbers //
+
+let x=18;
+let y=47;
+
+console.log(x+y); //Output:65//
+console.log(x-y); //Output:-29//
+console.log(x*y); //Output:846//
+console.log(x/y); //Output:0.3829787234042553//
+
+// 4.Strings //
+
+let fName = "Kalaguri";
+let lName = "Dinesh";
+console.log(fName+" "+lName); //Output:Kalaguri Dinesh//
+
+// 5.Booleans //
+
+let isJavaScriptFun = true;
+let isCodeHard = false;
+let isLearning = true;
+
+console.log(isJavaScriptFun && isCodeHard); //Output:false//
+console.log(isCodeHard || isLearning); //Output:true//
+
+// 6.Arrays //
+
+let myFavoriteFood =["Biryani","Dosa","Fish"];
+
+console.log(myFavoriteFood[0]); //Output:Biryani//
+console.log(myFavoriteFood.length);//Output:3//
+console.log(myFavoriteFood.push("Mushroom")); //Output:4//
+console.log(myFavoriteFood); //Output:[ 'Biryani', 'Dosa', 'Fish', 'Mushroom' ]//
+
+// 7.SimpleMath //
+
+let temperature = 35;
+console.log(temperature +5);//Output:40//
+console.log(temperature -2);//Output:32//
+console.log(temperature *3);//Output:105//
+console.log(temperature /2);//Output:17.5//
+
+// 8.Variable Names //
+
+// _firstName & $price  these two are valid//
+
+// 9.Comments //
+
+// A Single Comment //
+/* Multi line
+   comment */
+
+// 10. Data Type Checking //
+
+console.log(typeof "Hello World"); //Output:string//
+console.log(typeof 42); //Output:number//
+console.log(typeof true); //Output:boolean//
+console.log(typeof [1,2,3]); //Output:object//
+console.log(typeof undefined); //Output:undefined//
+
+// 11.Basic console //
+
+console.log("Hello, JavaScript!");
+console.info("Hello, JavaScript!");
+console.debug("Hello, JavaScript!");
+
+// 12.Variables and Numbers //
+
+let d= 18;
+let s= 47;
+
+console.log(d+s); //Output:65//
+console.log(d-s);  //Output:-29//
+console.log(d*s); //Output:846//
+console.log(d/s); //Output:0.3829787234042553//
+console.log(d/0); //Output:Infinity//
+console.log(s/0); //Output:Infinity//
+
+
+
